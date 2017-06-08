906e98e1e727a40bf6a9a1a981636e82a553788e
Cosimo Simeone <wiwwo@wiwwo.com>
2017-06-08 12:57:05 +0200

    This should be faster, sure it's better

--- btchain2graph.py
+++ btchain2graph.py
@@ -8,43 +8,52 @@ import time, datetime
 import gzip, sys, os, getopt
 import random
 
+DEBUG=0
+
 latestBlockUrl="https://blockchain.info/latestblock"
 rawBlockUrl="https://blockchain.info/rawblock/"
 
-### response = urllib2.urlopen(latestBlockUrl)
-
+# Get latest block
+response = urllib2.urlopen(latestBlockUrl)
 # TODO - This sucks, but "Need for Speed"
-### latestBlockAnswer = response.read()[15:81]
-
-### if latestBlockAnswer[0]+latestBlockAnswer[65] != '""': exit
+latestBlockHashAnswer = response.read()[15:81]
+if latestBlockHashAnswer[0]+latestBlockHashAnswer[65] != '""': exit
 
 # TODO - DEBUG
-#latestBlockAnswer = '"00000000000107925a52e24c838788a954d1a6d5858301c66d50b2a074787460"'
-#latestBlockAnswer = '"00000000000304d51de00dc9c22b439030160b615303f0978d0e5ebc14c643b3"'
-latestBlockAnswer = '"000000000002864e1165d5da2b9cf69512e96171f5d94c2ab1007c9a7933390b"'
-response = urllib2.urlopen(rawBlockUrl+latestBlockAnswer[1:65])
+#latestBlockHashAnswer = '"00000000000107925a52e24c838788a954d1a6d5858301c66d50b2a074787460"'
+#latestBlockHashAnswer = '"00000000000304d51de00dc9c22b439030160b615303f0978d0e5ebc14c643b3"'
+#latestBlockHashAnswer = '"000000000002864e1165d5da2b9cf69512e96171f5d94c2ab1007c9a7933390b"'
+latestBlockHashAnswer = '"0000000000000000013b476cf8af07a136c0cffd86ed6e268f5e6d4b02e4604f"'
+
+if DEBUG != 0: printMe( 'BLOCK HASH ->', latestBlockHashAnswer )
+if DEBUG != 0: print '-------------------------------------'
+#
+response = urllib2.urlopen(rawBlockUrl+latestBlockHashAnswer[1:65])
 rawBlockAnswer = response.read()
 
 jsonBlockAnswer=json.loads(rawBlockAnswer)
 
-print len(jsonBlockAnswer["tx"])
 transNum=0
 
 for transList in jsonBlockAnswer["tx"]:
-  print 'TRANS HASH ->', transList["hash"]
+  if DEBUG != 0: print 'TRANS HASH ->', transList["hash"]
   for transInput in transList["inputs"]:
-
     if transNum==0 :
-      print 'mine'
-      continue
+      transFrom='mine'
     else:
-      print 'IN ->',transInput["prev_out"]["addr"]
+      try:
+        transFrom=transInput["prev_out"]["addr"]
+      except: transFrom='--ERR--'
+    if DEBUG != 0: print 'IN ->',transFrom
 
   for transOut in transList["out"]:
-
-    print 'OUT ->',transOut["addr"]
-    print 'VAL ->',transOut["value"]
+    try:
+      transTo=transOut["addr"]
+      if DEBUG != 0: print 'OUT ->',transTo
+    except: transTo='--ERR--'
+    val=transOut["value"]
+    if DEBUG != 0: print 'VAL ->',val
 
   transNum=transNum+1
-  print '--------------------'
-print 'x'
+  if DEBUG != 0: print '--------------------'
+if DEBUG != 0: print "That's all folks!"
